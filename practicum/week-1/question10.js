46e406ae51f39c39e59446e1ce04f93a8bf62c87
Halle Jackson <hallejackson@Halles-MBP.attlocal.net>
2020-09-23 15:16:14 -0700

    question10 answer

--- practicum/week-1/question10.js
+++ practicum/week-1/question10.js
@@ -8,5 +8,10 @@ output: 7 - 2 = 5
 */
 
 var maxMinusMin = function(n) {
+    d = (n+"").split("")
+    sort(d)
+    let dnum0 = parseInt (d[0])
+    let dnum1 = parseInt (d[d.length-1])
+    return dnum1 - dnum0
 
 };
