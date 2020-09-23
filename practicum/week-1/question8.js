475e78e062b5bc44be858f7bd8c7749fd5b88436
Halle Jackson <hallejackson@Halles-MBP.attlocal.net>
2020-09-23 15:15:59 -0700

    question8 answer

--- practicum/week-1/question8.js
+++ practicum/week-1/question8.js
@@ -8,5 +8,25 @@ output: [1, 0, 1, 2]
 */
 
 var biggerAndEven = function(input) {
+    let counts = []
+    
+    
+    for (let i = 0; i < input.length; i += 1)
+    {
+        let count = 0
+        for (let j = 0 ; j < input.length; j += 1){
+            if (i == j){
+                continue
+            }
+            if (input[j] > input[i] && input[j]%2 == 0){
+                count += 1
+                
+            }
+
+            
+        }
+        counts.push(count)
+    }
+    return counts
 
 };
